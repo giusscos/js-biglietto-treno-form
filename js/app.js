978e5853b88dc60729d587272749327516a2ac4b
giusscos <giu.cosenza08@gmail.com>
2022-07-21 16:25:45 +0000

    Add result in page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const ticketTax = 0.21
-let fullName = '', distance = 0, ageSelect = 0, submit, ticketCost = 0, discount = 0;
+let fullName = '', distance = 0, ageSelect = 0, submit, ticketCost = 0, discount = 0, ticketOutPut;
 
 // Acquisizione elementi DOM
 fullName = document.getElementById('full_name');
@@ -9,6 +9,8 @@ submit = document.getElementById('submit');
 
 // Al click lavorazione dei dati acquisiti e stampa
 submit.addEventListener('click', function() {
+    ticketOutPut = document.querySelector('.ticket_item');
+
     // Costo del biglietto senza sconto
     ticketCost = Math.abs(distance.value) * ticketTax;
     
@@ -18,7 +20,7 @@ submit.addEventListener('click', function() {
     } else if (parseFloat(ageSelect.value) === 0.4){
         discount = ticketCost * 0.4;
     }
-    // Applicazione sconto al biglietto
+    // Applicazione sconto al biglietto e taglio a due posizioni dopo la virgola
     ticketCost -= discount;
+    ticketOutPut.innerHTML = ticketCost.toFixed(2);
 });
-
